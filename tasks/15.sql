c246e0f71b576c1a81cee1fb620fad3aa26a3e68
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2022-01-06 16:06:50 +0300

    fix task 15

--- tasks/15.sql
+++ tasks/15.sql
@@ -1,6 +1,12 @@
 -- Задание: 15 (Serge I: 2003-02-03)
 -- Найдите размеры жестких дисков, совпадающих у двух и более PC. Вывести: HD
 
+-- 1)
 SELECT DISTINCT p.hd
     FROM PC p
-    JOIN PC c ON p.hd = c.hd AND p.code != c.code
+    JOIN PC c ON p.hd = c.hd AND p.code != c.code;
+
+-- 2)
+SELECT hd FROM PC
+    GROUP BY hd
+    HAVING count(code) > 1;
